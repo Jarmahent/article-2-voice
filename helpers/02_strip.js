841e0d52102ea9ded0c2c8c4a849bc295cf28961
Kevin Hernandez <kevinhernandez@Kevins-MacBook-Pro-2.local>
2019-12-03 11:24:56 -0500

    added ability to export meta data

--- helpers/02_strip.js
+++ helpers/02_strip.js
@@ -1,7 +1,30 @@
 const striptags = require('striptags')
-module.exports = function(article){
-  console.log("Stripping...")
+const fs = require('fs')
+const path = require('path')
+
+module.exports = function(article, outDir, exportMeta){
   return new Promise((resolve, reject) =>{
-    return resolve(striptags(article.content))
+    tags_strippped = striptags(article.content)
+
+    if(exportMeta){
+      console.log("Stripping html entities and writing meta data...")
+      article_file = {
+        "url":article.url,
+        "content":tags_strippped,
+        "title":article.title,
+        "author":article.author,
+        "published":article.published,
+        "source": article.source
+      }
+  
+  
+      fs.writeFile(path.join(outDir, "meta.json"), JSON.stringify(article), (err)=>{
+        if(err){return reject(err)}
+        return resolve(striptags(tags_strippped))
+      })
+    }else{
+      console.log("Stripping...")
+      return resolve(striptags(tags_strippped))
+    }
   });
 }
--- helpers/05_write.js
+++ helpers/05_write.js
@@ -1,9 +1,12 @@
 const fs = require('fs')
+const path = require('path')
+
 module.exports = function(article, outDir){
   console.log("Writing...")
   return new Promise((resolve, reject) =>{
-    fs.writeFile(outDir, article, 'binary', (err)=>{
+    fs.writeFile(path.join(outDir, "article.mp3"), article, 'binary', (err)=>{
       if(err){return reject(err)}
+      console.log("Done!")
       return resolve(true)
     })
   })
